bf2de0219fe378b90525fd0ee7e70b0b10dfc2cd
dgutaev <dmgutaev@gmail.com>
2022-01-20 15:52:04 +0300

    Check about null in String

--- src/main/java/com/example/gbchat/HelloController.java
+++ src/main/java/com/example/gbchat/HelloController.java
@@ -2,13 +2,24 @@ package com.example.gbchat;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.util.Objects;
 
 public class HelloController {
+    String displayString;
+    public TextArea chatDisplay;
+    public TextField inputArea;
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onSendButtonClick() {
+        if (!Objects.equals(inputArea.getText(), "")) {
+            displayString = displayString + "\n" + inputArea.getText();
+            chatDisplay.setText(displayString);
+            inputArea.setText("");
+        }
     }
 }
