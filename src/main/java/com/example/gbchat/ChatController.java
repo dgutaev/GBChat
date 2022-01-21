627dba980461d9549902a8c141508739036790e7
dgutaev <dmgutaev@gmail.com>
2022-01-21 10:02:44 +0300

    Final

---
+++ src/main/java/com/example/gbchat/ChatController.java
@@ -0,0 +1,23 @@
+package com.example.gbchat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ChatController {
+    public TextArea chatDisplay;
+    public TextField inputArea;
+
+    @FXML
+    protected void onSendButtonClick() {
+        String stringMessage = inputArea.getText();
+        if (stringMessage != null && !stringMessage.isEmpty()) {
+            if (chatDisplay.getText().isEmpty()) {
+                chatDisplay.setText(stringMessage);
+            } else {
+                chatDisplay.setText(chatDisplay.getText() + "\n" + stringMessage);
+                inputArea.setText("");
+            }
+        }
+    }
+}
